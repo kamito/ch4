b72932569d5f1d131e3b32166d2582b8966e4de5
Shinichirow KAMITO <s-kamito@groovenauts.jp>
2016-08-10 10:52:01 +0900

    Add specs.

--- spec/store/get.test.js
+++ spec/store/get.test.js
@@ -37,4 +37,63 @@ describe("store", () => {
       assert(store.get('test2') === initState.test2);
     });
   });
+
+  describe("set", () => {
+    let store = null;
+    let initState = {
+      test1: {
+        test1_1: {
+          test1_1_1: "test1-1-1",
+          test1_1_2: "test1-1-2"
+        },
+        test1_2: {
+          test1_2_1: "test1-2-1",
+          test1_2_2: "test1-2-2"
+        }
+      },
+      test2: "test2"
+    };
+    before(() => {
+      store = createStore('spec', initState);
+    });
+    after(() => {
+      destroyAllStores();
+    });
+
+    it("set string", () => {
+      store.set('test1.test1_1.test1_1_1', 'changed value');
+      assert(store.get('test1.test1_1.test1_1_1') === 'changed value');
+    });
+
+    it("set number", () => {
+      store.set('test1.test1_1.test1_1_2', 1000);
+      assert(store.get('test1.test1_1.test1_1_2') === 1000);
+    });
+
+    it("set array", () => {
+      store.set('test1.test1_2.test1_2_1', ["a", "b", "c"]);
+      assert.deepEqual(store.get('test1.test1_2.test1_2_1'), ["a", "b", "c"]);
+    });
+
+    it("set null", () => {
+      store.set('test1.test1_2.test1_2_2', null);
+      assert(store.get('test1.test1_2.test1_2_2') === null);
+    });
+
+    it("set object", () => {
+      let data = {
+        test1: {
+          test1_1: {
+            test1_1_1: "changed",
+            test1_1_2: 1000
+          }
+        }
+      };
+      store.set(data);
+      assert(store.get('test1.test1_1.test1_1_1') === "changed");
+      assert(store.get('test1.test1_1.test1_1_2') === 1000);
+    });
+
+  });
+
 });
--- spec/store/on.test.js
+++ spec/store/on.test.js
@@ -30,26 +30,137 @@ describe("store", () => {
   });
 
   describe("connect", () => {
-    let store = null;
-    let initState = { test: "test" };
-    before(() => {
-      store = createStore('spec', initState);
-    });
-    after(() => {
-      destroyAllStores();
+    describe("shallow state", () => {
+      let store = null;
+      let initState = { test: "test" };
+      before(() => {
+        store = createStore('spec', initState);
+      });
+      after(() => {
+        destroyAllStores();
+      });
+
+      it("call the connected function", (done) => {
+        store.connect((store_) => {
+          assert(store_.get('test') === 'test1');
+          done();
+        });
+        store.on('spec', (store_, ...args) => {
+          assert(store_.name === 'spec');
+          assert(args[0] === 'test1');
+          return { test: args[0] };
+        });
+        dispatcher.trigger('spec', 'test1');
+      });
     });
 
-    it("call the connected function", (done) => {
-      store.connect((store_) => {
-        assert(store_.get('test') === 'test1');
-        done();
+    describe("deep state", () => {
+      let store = null;
+      let initState = {
+        test1: {
+          test1_1: {
+            test1_1_1: "test1-1-1",
+            test1_1_2: "test1-1-2"
+          },
+          test1_2: {
+            test1_2_1: "test1-2-1",
+            test1_2_2: "test1-2-2"
+          }
+        },
+        test2: "test2"
+      };
+      before(() => {
+        store = createStore('spec', initState);
       });
-      store.on('spec', (store_, ...args) => {
-        assert(store_.name === 'spec');
-        assert(args[0] === 'test1');
-        return { test: args[0] };
+      after(() => {
+        destroyAllStores();
       });
-      dispatcher.trigger('spec', 'test1');
+
+      it("call the connected function", (done) => {
+        store.connect((store_) => {
+          assert(store_.get('test1.test1_1.test1_1_1') === 'test1-1-1');
+          assert(store_.get('test1.test1_1.test1_1_2') === 'changed value');
+          assert(store_.get('test1.test1_2.test1_2_1') === 'test1-2-1');
+          assert(store_.get('test1.test1_2.test1_2_2') === 'test1-2-2');
+          assert(store_.get('test2') === 'test2');
+          done();
+        });
+        store.on('spec', (store_, ...args) => {
+          assert(store_.name === 'spec');
+          assert(args[0] === 'test1');
+          return { test1: { test1_1: { test1_1_2: "changed value" } } };
+        });
+        dispatcher.trigger('spec', 'test1');
+      });
+    });
+
+    describe("any value", () => {
+      let store = null;
+      let initState = {
+        itString: "test",
+        itNumber: 1000,
+        itStringToNull: "test",
+        itStringToUndefined: "test",
+        itArray: ["a", "b", "c"]
+      };
+      before(() => {
+        store = createStore('spec', initState);
+        store.on('spec/string', (store_, ...args) => {
+          return { itString: "change to string" };
+        });
+        store.on('spec/number', (store_, ...args) => {
+          return { itNumber: 100 };
+        });
+        store.on('spec/toNull', (store_, ...args) => {
+          return { itStringToNull: null };
+        });
+        store.on('spec/toUndefined', (store_, ...args) => {
+          return { itStringToUndefined: undefined };
+        });
+        store.on('spec/array', (store_, ...args) => {
+          return { itArray: ["x", "y", "z"] };
+        });
+      });
+      after(() => {
+        destroyAllStores();
+      });
+
+      it("string changed value", () => {
+        store.connect((store_) => {
+          assert.deepEqual(store_.get('itString'), "change to string");
+        });
+        dispatcher.trigger('spec/string');
+      });
+
+      it("array changed value", () => {
+        store.connect((store_) => {
+          assert.deepEqual(store_.get('itArray'), ["x", "y", "z"]);
+        });
+        dispatcher.trigger('spec/array');
+      });
+
+      it("number changed value", () => {
+        store.connect((store_) => {
+          assert.deepEqual(store_.get('itNumber'), 100);
+        });
+        dispatcher.trigger('spec/number');
+      });
+
+      it("value changed to null", () => {
+        store.connect((store_) => {
+          assert.deepEqual(store_.get('itStringToNull'), null);
+        });
+        dispatcher.trigger('spec/toNull');
+      });
+
+      it("value did not change to undefined", () => {
+        store.connect((store_) => {
+          assert.notDeepEqual(store_.get('itStringToUndefined'), undefined);
+          assert.deepEqual(store_.get('itStringToUndefined'), 'test');
+        });
+        dispatcher.trigger('spec/toUndefined');
+      });
+
     });
   });
 });
